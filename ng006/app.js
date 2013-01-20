2be7dd5e865fcc110074355a8d40565190a016e6
Sam Lin <usesam@gmail.com>
2013-01-19 23:45:26 -0600

    change objectEquality to true

--- ng006/app.js
+++ ng006/app.js
@@ -2,15 +2,15 @@ var app = angular.module('ngApp', [])
     .directive('xngWatcher', function () {
         "use strict";
         return {
-            template: '<span>X:{{x}}, Y:{{y}}</span>',
+            template: '<span>X:{{x1}}, Y:{{y1}}</span>',
             link: function (scope, elm, iAttrs) {
                 scope.$watch(function () {
                     return {x: iAttrs.x, y: iAttrs.y};
                 }, function (newVal, oldVal, scope) {
                     console.log('change !');
-                    scope.x = newVal.x;
-                    scope.y = newVal.y;
-                }, false);
+                    scope.x1 = newVal.x;
+                    scope.y1 = newVal.y;
+                }, true);
             }
         };
     });
